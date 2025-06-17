7a2a831bd2e716647f06e7481f98f4ddca006368
erdoslibrary <book419@gmail.com>
2025-06-17 18:52:31 +0900

    Add: 저장안 된 파일 마저 올림

--- level4/sort_int_tab.c
+++ level4/sort_int_tab.c
@@ -25,28 +25,28 @@ void sort_int_tab(int *tab, unsigned int size)
 		i++;
 	}
 }
-// 여기서부터는 확인용
-void print_tab(int *tab, unsigned int size)
-{
-	unsigned int i = 0;
-	while (i < size)
-	{
-		printf("%d", tab[i]);
-		i++;
-	}
-	printf("\n");
-}
+// // 여기서부터는 확인용
+// void print_tab(int *tab, unsigned int size)
+// {
+// 	unsigned int i = 0;
+// 	while (i < size)
+// 	{
+// 		printf("%d", tab[i]);
+// 		i++;
+// 	}
+// 	printf("\n");
+// }
 
-int main(void)
-{
-	int arr[] = {5,2,3,4,5,1};
-	unsigned int size = 6;
+// int main(void)
+// {
+// 	int arr[] = {5,2,3,4,5,1};
+// 	unsigned int size = 6;
 
-	printf("before: ");
-	print_tab(arr, size);
-	sort_int_tab(arr, size);
+// 	printf("before: ");
+// 	print_tab(arr, size);
+// 	sort_int_tab(arr, size);
 
-	printf("after: ");
-	print_tab(arr, size);
-	return 0;
-}
+// 	printf("after: ");
+// 	print_tab(arr, size);
+// 	return 0;
+// }
--- level4/sort_list.c
+++ level4/sort_list.c
@@ -29,56 +29,56 @@ t_list *sort_list(t_list *lst, int (*cmp)(int, int))
 	return lst;
 }
 
-int ascending(int a, int b)
-{
-	return (a <= b);
-}
+// int ascending(int a, int b)
+// {
+// 	return (a <= b);
+// }
 
-t_list *create_node(int data)
-{
-	t_list *new_node = (t_list *)malloc(sizeof(t_list));
-	if (!new_node)
-		return NULL;
-	new_node->data = data;
-	new_node->next = NULL;
-	return new_node;
-}
+// t_list *create_node(int data)
+// {
+// 	t_list *new_node = (t_list *)malloc(sizeof(t_list));
+// 	if (!new_node)
+// 		return NULL;
+// 	new_node->data = data;
+// 	new_node->next = NULL;
+// 	return new_node;
+// }
 
-void print_list(t_list *lst)
-{
-	while (lst)
-	{
-		printf("%d ", lst->data);
-		lst = lst->next;
-	}
-	printf("\n");
-}
+// void print_list(t_list *lst)
+// {
+// 	while (lst)
+// 	{
+// 		printf("%d ", lst->data);
+// 		lst = lst->next;
+// 	}
+// 	printf("\n");
+// }
 
-int main(void)
-{
-	t_list *a = create_node(5);
-	t_list *b = create_node(3);
-	t_list *c = create_node(7);
-	t_list *d = create_node(1);
+// int main(void)
+// {
+// 	t_list *a = create_node(5);
+// 	t_list *b = create_node(3);
+// 	t_list *c = create_node(7);
+// 	t_list *d = create_node(1);
 
-	a->next = b;
-	b->next = c;
-	c->next = d;
-	d->next = NULL;
+// 	a->next = b;
+// 	b->next = c;
+// 	c->next = d;
+// 	d->next = NULL;
 
-	printf("Before sorting: ");
-	print_list(a);
+// 	printf("Before sorting: ");
+// 	print_list(a);
 
-	t_list *sorted = sort_list(a, ascending);
-	printf("After sorting: ");
-	print_list(sorted);
+// 	t_list *sorted = sort_list(a, ascending);
+// 	printf("After sorting: ");
+// 	print_list(sorted);
 
-	while (sorted)
-	{
-		t_list *temp = sorted;
-		sorted = sorted->next;
-		free(temp);
-	}
-	return 0;
-}
+// 	while (sorted)
+// 	{
+// 		t_list *temp = sorted;
+// 		sorted = sorted->next;
+// 		free(temp);
+// 	}
+// 	return 0;
+// }
 
